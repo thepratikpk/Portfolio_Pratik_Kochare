f44c7f1bc0a56ef09e8e8b36447395d3c005ee70
thepratikpk <thepratikpk@gmail.com>
2025-05-03 18:35:32 +0530

    cors update

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,9 @@ import videoRouter from './routes/video.route.js'
 const app=express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true
-}))
+  }));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
