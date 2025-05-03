af2e750178bd8e1f46b2acae9fe3dbf672a2883c
thepratikpk <thepratikpk@gmail.com>
2025-05-03 17:50:58 +0530

    chanes for .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
+
+
 // https://vite.dev/config/
-export default defineConfig({
-  server:{
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+  return {
+
+    server:{
       proxy:{
-        "/api":"http://localhost:3000"
+        "/api":{
+          target: env.VITE_BACKEND_URI,
+          changeOrigin: true,
+          secure: false,
+        }
+
       }
-  },
-  plugins: [react(),
-    tailwindcss(),
-  ],
+    },
+    plugins: [react(),
+      tailwindcss(),
+    ],
+  }
 })
